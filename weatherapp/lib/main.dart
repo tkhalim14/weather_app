10be9fd34c0456f49b253d4ef0e98ad642fe4746
Tabish Khalid Halim <tkhalim14@gmail.com>
2022-09-10 02:15:26 +0530

    Renamed Title

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Weather App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
